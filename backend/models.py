56af77aed339d9efe1b7cf51228ee01fd9caa8d3
vutrngkien <vutrungkien2k@gmail.com>
2023-07-23 00:27:33 +0700

    add test

--- backend/models.py
+++ backend/models.py
@@ -3,7 +3,7 @@ from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
 
-database_path = os.getenv('DB_PATCH')
+database_path = os.getenv('DB_PATH')
 
 db = SQLAlchemy()
 
--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -14,8 +14,7 @@ class TriviaTestCase(unittest.TestCase):
         """Define test variables and initialize app."""
         self.app = create_app()
         self.client = self.app.test_client
-        self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = os.getenv('DB_PATH_TEST')
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -33,6 +32,13 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+    def test_get_questions(self):
+        res = self.client().get('/questions?page=1')
+        self.assertTrue(res.__dict__['_status_code'], 200)
+        
+    def test_get_questions_fail(self):
+        res = self.client().get('/questions?page=-1')
+        self.assertEqual(res.__dict__['_status_code'], 404)
 
 
 # Make the tests conveniently executable
